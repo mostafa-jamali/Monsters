f19e5aec3317a9bc25adfb14c28597f884b0c2c3
mostafa jamali <m.jamali20274@gmail.com>
2020-07-18 00:35:44 +0430

    Updates

---
+++ precache-manifest.6e517e9fa50b25b81ba49530d0d4c8be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ac5e22fed1bbb1105410a84f8a70269",
+    "url": "/Monsters/index.html"
+  },
+  {
+    "revision": "ee553b681f2c505e738f",
+    "url": "/Monsters/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "7695d37dc99a1afd37e4",
+    "url": "/Monsters/static/css/main.976ff004.chunk.css"
+  },
+  {
+    "revision": "ee553b681f2c505e738f",
+    "url": "/Monsters/static/js/2.fc7abcc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters/static/js/2.fc7abcc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7695d37dc99a1afd37e4",
+    "url": "/Monsters/static/js/main.698c1f9c.chunk.js"
+  },
+  {
+    "revision": "5786516ee603904cc4f8",
+    "url": "/Monsters/static/js/runtime-main.5f155032.js"
+  }
+]);
